5fff7baa69d3f4b619abe931380778c26ff95193
apoorv <apoorv.patne10@gmail.com>
2018-09-08 23:19:31 +0530

    added DeleteView

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     PostListView, PostDetailView, PostCreateView,
-    PostUpdateView
+    PostUpdateView, PostDeleteView
 )
 from . import views
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
